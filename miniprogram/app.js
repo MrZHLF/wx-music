9d3c6f86f1fb3e0896d35bbc3b3d9d12107f56ac
MrzHLF <480970024@qq.com>
2020-03-15 14:08:24 +0800

    版本更新

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,7 +1,7 @@
 //app.js
 App({
   onLaunch: function () {
-    
+    this.checkUpate()
     if (!wx.cloud) {
       console.error('请使用 2.2.3 或以上的基础库以使用云能力')
     } else {
@@ -38,4 +38,25 @@ App({
       }
     })
   },
+
+  checkUpate(){
+    const updateManager = wx.getUpdateManager()
+    // 检测版本更新
+    updateManager.onCheckForUpdate((res) =>{
+      if(res.hasUpdate) {
+        updateManager.onUpdateReady(()=>{
+          wx.showModal({
+            title: '更新提示',
+            content: '新版本已经更新，是否重启应用',
+            success(res) {
+              if(res.confirm) {
+                // 点击确定按钮
+                updateManager.applyUpdate()
+              }
+            }
+          })
+        })
+      }
+    })
+  }
 })
